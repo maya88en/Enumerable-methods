0cff463195782b6a9549ecfe0949934dd5aa2aa8
maya88en <maya88en@gmail.com>
2019-11-06 12:35:52 +0200

    fix bugs in the methods

--- method.rb
+++ method.rb
@@ -23,28 +23,65 @@ module Enumerable
     result
   end
 
-  def my_all?
+  def my1_all?
+    return true unless block_given?
     my_each { |element| return false if yield(element) == false }
     true
   end
 
-  def my_any?
+  def my_all?(pattern = nil)
+    if block_given?
+      my_each { |v| return false unless yield(v) }
+    elsif !pattern.nil?
+      my_each { |v| return false unless pattern ==   (v.to_i) }
+    else
+      my_each { |v| return false unless v }
+    end
+    true
+  end
+
+  def my1_any?
+    return true unless block_given?
     my_each { |element| return true if yield(element) }
     false
   end
 
-  def my_none?
+  def my_any?(pattern = nil)
+    if block_given?
+      my_each { |v| return true if yield(v) }
+    elsif !pattern.nil?
+      my_each { |v| return true if pattern == (v.to_i) }
+    else
+      my_each { |v| return true if v }
+      return false
+    end
+    false
+  end
+
+  def my1_none?
+    return true unless block_given?
     result = true
     each { |element| result = false if yield(element) }
     result
   end
 
-  def my_count(x = nil)
+  def my_none?(pattern = nil)
+    if block_given?
+        my_each { |v| return false if yield(v) }
+    elsif !pattern.nil?
+      my_each { |v| return false if pattern == (v.to_i) }
+    else
+      my_each { |v| return false if v }
+    end
+    true
+  end
+
+  def my_count(num = nil)
     count = 0
     if block_given?
       my_each { |el| count += 1 if yield el }
-    elsif x
-      my_each { |el| count += 1 if el == x }
+    elsif num
+      my_each { |el| count += 1 if el == num }
     else
       count = length
     end
@@ -70,4 +107,11 @@ module Enumerable
   def multiply_els
     my_inject(1) { |x, y| x * y }
   end
-  end
+end
+
+arr=[1,2,3,4]
+puts arr.my_all?(3)
+
+puts arr.my_any?(5)
+
+puts arr.my_none?(5)
