e689a2e620a3e8722d3d92c9d4921769711de1fe
maya88en <maya88en@gmail.com>
2019-11-18 22:58:17 +0200

    fix errors

--- method.rb
+++ method.rb
@@ -2,7 +2,7 @@
 
 module Enumerable
   def my_each
-    return true unless block_given?
+    return to_enum unless block_given?
 
     (0...length).each do |i|
       yield(self[i])
@@ -10,7 +10,7 @@ module Enumerable
   end
 
   def my_each_with_index
-    return true unless block_given?
+    return to_enum unless block_given?
 
     k = 0
     (0...length).each do |i|
@@ -20,7 +20,7 @@ module Enumerable
   end
 
   def my_select(&block)
-    return true unless block_given?
+    return to_enum unless block_given?
 
     result = []
     each do |element|
@@ -30,35 +30,64 @@ module Enumerable
   end
 
   def my_all?(pattern = nil)
-    if block_given?
-      my_each { |v| return false unless yield(v) }
-    elsif !pattern.nil?
-      my_each { |v| return false unless pattern == v.to_i }
-    else
-      my_each { |v| return false unless v }
+    i = 0
+    ar = self
+
+    while i < ar.size
+      if block_given? == true
+        return false unless yield(ar[i])
+      elsif pattern.class == Class
+        return false unless ar[i].class.ancestors.include? pattern
+      elsif pattern.class == Regexp
+        return false unless ar[i] =~ pattern
+      elsif pattern.nil? == true
+        return false unless ar[i]
+      else
+        return false unless pattern[i] == ar[i]
+      end
+      i += 1
     end
     true
   end
 
   def my_any?(pattern = nil)
-    if block_given?
-      my_each { |v| return true if yield(v) }
-    elsif !pattern.nil?
-      my_each { |v| return true if pattern == v.to_i }
-    else
-      my_each { |v| return true if v }
-      return false
+    i = 0
+    ar = self
+    while i < ar.size
+      if block_given? == true
+        return true if yield(ar[i])
+      elsif pattern.class == Class
+        return true if ar[i].class.ancestors.include? pattern
+      elsif pattern.class == Regexp
+        return true if ar[i] =~ pattern
+      elsif pattern.nil? == true
+        return true if ar[i]
+      elsif pattern[i] == ar[i]
+        return true
+      end
+
+      i += 1
     end
     false
   end
 
   def my_none?(pattern = nil)
-    if block_given?
-      my_each { |v| return false if yield(v) }
-    elsif !pattern.nil?
-      my_each { |v| return false if pattern == v.to_i }
-    else
-      my_each { |v| return false if v }
+    i = 0
+    ar = self
+    while i < ar.size
+      if block_given? == true
+        return false if yield (ar[i])
+      elsif pattern.class == Class
+        return false if ar[i].class.ancestors.include? pattern
+      elsif pattern.class == Regexp
+        return false if ar[i] =~ pattern
+      elsif pattern.nil? == true
+        return false if ar[i]
+      elsif pattern[i] == ar[i]
+        return false
+      end
+
+      i += 1
     end
     true
   end
@@ -76,7 +105,7 @@ module Enumerable
   end
 
   def my_map(&block)
-    return true unless block_given?
+    return to_enum unless block_given?
 
     result = []
     my_each do |i|
@@ -103,6 +132,46 @@ module Enumerable
 end
 
 arr = [1, 2, 3, 4]
+
+#puts [1,2,3,4].my_each
+
+#puts [1,2,3,4].my_each_with_index
+
+#puts [1,2,3,4].my_select
+
+#puts [1,2,3,4].my_map
+
+# puts [1, 3.14, 42].my_all?(Integer)
+# puts [1, 2, 3, 4].my_all?(Integer)
+
+# puts ['ant', 'bear', 'cat'].my_all?(/t/)
+
+# puts ['ant', 'tuna', 'cat'].my_all?(/t/)
+
+# puts [1, 3.14, 42].my_any?(Integer)
+
+# puts ['text', true, nil, 4.32].my_any?(Integer)
+
+# puts ['ant', 'bear', 'boy'].my_any?(/t/)
+
+# puts ['car', 'bear', 'boy'].my_any?(/t/)
+
+#p [1,2,3,4].my_any?(4)
+
+#p [1,2,3,5].my_any?(4)
+
+# puts [1, 3.14, 42].my_none?(Integer) 
+
+#puts [1, 3.14, 42].my_none?(String)
+
+# p ['ant', 'bear', 'boy'].my_none?(/t/)
+
+#p ['car', 'bear', 'boy'].my_none?(/t/)
+
+#p [5, 6, 7, 8, 9, 10].inject { |sum, n| sum + n }
+
+#p [5, 6, 7, 8, 9, 10].inject(:+)
+
 # puts arr.my_all?(3)
 
 # puts arr.my_any?(5)
@@ -113,10 +182,10 @@ arr = [1, 2, 3, 4]
 
 # puts arr.my_inject(:+)
 
-puts arr.my_count
+#puts arr.my_count
 
-puts arr.my_inject{ |final_val, x| final_val + x }
+#puts arr.my_inject{ |final_val, x| final_val + x }
 #puts arr.my_inject { |final_val, x| final_val + x }
-puts arr.count
+#puts arr.count
 
 # puts arr.count == arr.my_count
